6bcc476f9c0bae49334d7e840c1b89859e7d7498
Sangay Wangchuk <sangaywangchuk100@gmail.com>
2022-12-06 17:35:35 +0600

    eslint setup and action setup

--- .github/actions/src/index.ts
+++ .github/actions/src/index.ts
@@ -2,17 +2,17 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import eslintJsonReportToJsObject from './eslintReportJsonToObject';
 import inputs from './inputs';
+
 (async () => {
   /**
    * get User inputs
    */
 
-  const { sha, githubContext, owner, repo, checkName, eslintReportFile } = inputs;
-  const parsedEslintReportJs = eslintJsonReportToJsObject(eslintReportFile);
-  core.debug(`Starting analysis of the ESLint report json to javascript object`);
-  console.log('report2', parsedEslintReportJs);
-
   try {
+    const { sha, githubContext, owner, repo, checkName, eslintReportFile } = inputs;
+    const parsedEslintReportJs = eslintJsonReportToJsObject(eslintReportFile);
+    core.debug(`Starting analysis of the ESLint report json to javascript object`);
+    console.log('report2', parsedEslintReportJs);
     core.notice('github action');
   } catch (e) {
     const error = e as Error;
