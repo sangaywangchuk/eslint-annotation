40a40bcea3fa831eb46d77e5cd3b95a55221bc93
Sangay Wangchuk <sangaywangchuk100@gmail.com>
2022-12-15 17:15:14 +0600

    pull request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Compasdfonent } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eslint-annotation';
+  asdfsdfasdf
+  constructor() {
+    
+  }
 }
