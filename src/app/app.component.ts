c54843a23bb8dc94529aa0fd5e1738831fd5bb06
Sangay Wangchuk <sangaywangchuk100@gmail.com>
2022-12-22 16:04:09 +0600

    testing eslint on pR files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eslint-annotation';
-
-  time() {
-    title = 'sadfasdf'
-  }
 }
--- src/app/eslint/eslint.component.ts
+++ src/app/eslint/eslint.component.ts
@@ -1,15 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
-import { lksdlkew } from '@angular/core';
-
 
 @Component({
   selector: 'app-eslint',
   templateUrl: './eslint.component.html',
   styleUrls: ['./eslint.component.scss']
 })
-export class EslintComponent implements OnInit {
-
-  constructor() {}
-
-}
+export class EslintComponent {}
